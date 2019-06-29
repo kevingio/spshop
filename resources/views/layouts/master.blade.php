b5d9a30e5b7252e2d4f736644dc5b00044af3f26
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-06-29 16:06:07 +0700

    serve assets from https

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<link rel="icon" type="image/png" href="{{ asset('app-asset/images/icons/favicon.png') }}"/>
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+	<link rel="icon" type="image/png" href="{{ asset('app-asset/images/icons/favicon.png', true) }}"/>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css', true) }}">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 </head>
@@ -25,7 +25,7 @@
 	<div id="dropDownSelect1"></div>
     <div id="dropDownSelect2"></div>
 
-	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/app.js', true) }}"></script>
 
 </body>
 </html>
